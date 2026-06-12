1a674eae23320aaf48dfdc74fabded8a495900bb
naomi-oleary <oleary.ng@gmail.com>
2026-06-11 19:09:36 -0600

    removed second map from NftCard

--- src/components/UI/NftCard.jsx
+++ src/components/UI/NftCard.jsx
@@ -5,60 +5,52 @@ import NewItemsCountdownTimer from "./NewItemsCountdownTimer";
 const NftCard = ({ items }) => {
     
     return (
-        <div>
-            {items.length > 0 ? (
-                items.map((item) => (
-                <div className="nft__item" key={item.id}>
-                    <div className="author_list_pp">
-                        <Link to="/authors">
-                        <img className="lazy" src={item.authorImage} alt="" />
-                        <i className="fa fa-check"></i>
-                        </Link>
-                    </div>
-                    <div className="newItems__countdownTimer">
-                        <NewItemsCountdownTimer expiryDate={item.expiryDate} />
-                    </div>
-                    <div className="nft__item_wrap">
-                        <div className="nft__item_extra">
-                            <div className="nft__item_buttons">
-                                <button>Buy Now</button>
-                                <div className="nft__item_share">
-                                    <h4>Share</h4>
-                                    <a href="" target="_blank" rel="noreferrer">
-                                        <i className="fa fa-facebook fa-lg"></i>
-                                    </a>
-                                    <a href="" target="_blank" rel="noreferrer">
-                                        <i className="fa fa-twitter fa-lg"></i>
-                                    </a>
-                                    <a href="">
-                                        <i className="fa fa-envelope fa-lg"></i>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <Link to="/item-details">
-                            <img
-                                src={item.nftImage}
-                                className="lazy nft__item_preview"
-                                alt=""
-                            />
-                        </Link>
-                    </div>
-                    <div className="nft__item_info">
-                        <Link to="/item-details">
-                            <h4>{item.title}</h4>
-                        </Link>
-                        <div className="nft__item_price">{item.price}</div>
-                        <div className="nft__item_like">
-                            <i className="fa fa-heart"></i>
-                            <span>{item.likes}</span>
+        <div className="nft__item" key={items.id}>
+            <div className="author_list_pp">
+                <Link to="/authors">
+                <img className="lazy" src={items.authorImage} alt="" />
+                <i className="fa fa-check"></i>
+                </Link>
+            </div>
+            <div className="newItems__countdownTimer">
+                <NewItemsCountdownTimer expiryDate={items.expiryDate} />
+            </div>
+            <div className="nft__item_wrap">
+                <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                                <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                                <i className="fa fa-envelope fa-lg"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
-            ))
-            ) : (
-                <p>no items found</p>
-            )}
+                <Link to="/item-details">
+                    <img
+                        src={items.nftImage}
+                        className="lazy nft__item_preview"
+                        alt=""
+                    />
+                </Link>
+            </div>
+            <div className="nft__item_info">
+                <Link to="/item-details">
+                    <h4>{items.title}</h4>
+                </Link>
+                <div className="nft__item_price">{items.price}</div>
+                <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>{items.likes}</span>
+                </div>
+            </div>
         </div>
     );
 };
